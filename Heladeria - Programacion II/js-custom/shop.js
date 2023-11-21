4df8767ae2e4ca28db216822d51ec99fc459cc58
Facundo Perea Fernández <utn.facundopff@gmail.com>
2023-11-21 09:56:35 -0300

    Arreglando las cagadas Ignacio

--- Heladeria - Programacion II/js-custom/shop.js
+++ Heladeria - Programacion II/js-custom/shop.js
@@ -1,315 +1,139 @@
-// Cart Toggle
-let cartToggle = document.getElementById('cart-button');
-let cartToggleCount = 0;
-let cartWrapper = document.getElementById('cart-wrapper');
-let cartElement = document.getElementById('cart');
-let subtotal = document.getElementById('subtotal');
-let total = document.getElementById('total');
-let applyPromoButton = document.querySelector('#apply-promo');
-let addToCartButton = document.querySelectorAll('.add-to-cart');
+(function(window){
+	var addButtons = document.querySelectorAll('.item-add');
+	var shopCart = document.querySelector('.shop-cart');
 
-applyPromoButton.addEventListener('click', applyPromo);
+	var cartPricing = {
+		cartList: {},
+		priceTotal: 0,
+		promoSavings: 0,
+		addItem: function(id, price, quantity){
+			this.cartList[id] = {"price":+price, "quantity": quantity};
+			this.updatePrice();
+		},
+		removeItem: function(id){
+			delete this.cartList[id];
+			this.updatePrice();
+		},
+		updatePrice: function(){
+			var sum = 0;
+			for(id in this.cartList){
+			sum += this.cartList[id].price * this.cartList[id].quantity;
 
-let cartProductQuantity = "<input name='quantity' id='quantity-value' type='number' value='1'>";
-
-let productRemove = "<button class='remove' onclick='removeFromCart(this)'>X</button>";
-
-let updatedPrice;
-let added = [], itemClass = {};
-
- // Capture HTML cart body
-var cartTableBody = document.getElementById('cart-table-body');
-
-// Cart object model
-let cart = {
-    'items': [],
-    "subtotal": 0,
-	"iphoneDis": 0,
-	"androidDis": 0,
-	"Off5": 0,
-	"finalDis": 0,
-    'total': 0
-};
-
-for (let button of addToCartButton) {
-    button.addEventListener('click', addToCart);
-}
-
-
-$('.trigger, .keep-shopping').click(function() {
-  $('.slider').toggleClass('close');
-});
-
-// Adding products to the shopping cart
-function addToCart(event) {
-    
-    if (added.includes(event.target.parentElement.id)) {
-        duplicateId = '#' + event.target.parentElement.id;
-        
-        cartTableBody.querySelector(duplicateId).querySelector('#quantity-value').value++;
-        return;
-    }
-    
-    // Capture product
-    let product = event.target.parentNode;
-    let productId = product.id;
-    added.unshift(productId);
-    let productName = product.querySelector('.product-name').innerHTML;
-    let productPrice = product.querySelector('.product-price').innerHTML.replace(/[^\d.-]/g, '');
-    productPrice = Number(productPrice);
-    let productUpdatedPrice = productPrice;
-    
-    thisClass = product.classList.value.split(' ');
-/*
-    thisClass = thisClass.replace('products ', '');
-*/
-    
-    itemClass[productId] = thisClass;
-    
-    // Add item to cart javascript model
-    cart.items.push({
-		'product': productId,
-		'productName': productName,
-		'productPrice': productPrice,
-		'productUpdatedPrice': productUpdatedPrice,
-	});
-    
-    
-    // Add a new row for each product
-    let productRow = document.createElement('tr');
-    productRow.setAttribute('id', productId);
-    cartTableBody.appendChild(productRow);
-    
-    // Add five columns to product row
-    for (let num = 0; num <= 6; num++) {
-        let newColumn = document.createElement('td');
-        productRow.appendChild(newColumn);
-    }
-            
-    // Build HTML cart
-    productRow.childNodes[0].innerHTML = productName;
-    productRow.childNodes[1].innerHTML = cartProductQuantity;
-    productRow.childNodes[1].setAttribute('id', 'quantity');
-    productRow.childNodes[2].innerHTML = productPrice;
-    productRow.childNodes[2].setAttribute('id', 'product-price');
-    productRow.childNodes[2].setAttribute('class', 'cart-product-price');
-    productRow.childNodes[3].innerHTML = productPrice;
-    productRow.childNodes[3].setAttribute('id', 'updated-product-price');
-    productRow.childNodes[3].setAttribute('class', 'cart-updated-product-price');
-    productRow.childNodes[4].innerHTML = "<button id='update' onclick='updateQuantity(this)'>Update</button>"
-    productRow.childNodes[5].innerHTML = productRemove;
-    
-    
-    
-    updateSubtotal();
-    updateTotal();
-    
-    $('.slider').toggleClass('close');
-    
-}
-
-function removeFromCart(event) {
-    let parentRow = event.parentNode.parentNode;
-    let parentBody = parentRow.parentNode;
-    let parentRowId = parentRow.id;
-    parentBody.removeChild(parentRow);
-    
-    total.value = (Number(total.value) - parentRow.querySelector('#updated-product-price').innerHTML).toFixed(2);
-    
-    var index = added.indexOf(parentRowId);
-    if (index !== -1) {
-        added.splice(index, 1);
-    }
-    
-    for (let item of cart.items) {
-        if (item.product === parentRowId) cart.items.splice(item, 1);
-    }
-    
-    var subtotalRow = document.getElementById("subtotal");
-	var subtotalRowParent = subtotalRow.parentNode;
-	for (let node of subtotalRowParent.childNodes) {
-		if (node.id === "discountRow") {
-            var discountRow = document.getElementById("discountRow");
-            
-            subtotalRowParent.removeChild(discountRow);
-        }
-
+			}this.cartList[id]
+			this.priceTotal = sum;
+		},
+		applyPromo: function(code){
+			var promoPrice = 0;
+			// check for promocode
+			if(code == "singles"){
+				let item = 0;
+				for (i in this.cartList){
+					if(this.cartList[i].price > item)
+						item = this.cartList[i].price;
+				}
+				promoPrice = item/2;
+			}
+			else if(code == "sale"){
+				promoPrice = (this.priceTotal - this.priceTotal * .9).toFixed(2);
+			}
+			//Check if applied promo is bettern than current.
+			if(promoPrice > this.promoSavings){
+				this.priceTotal = this.priceTotal + this.promoSavings - promoPrice;
+				this.promoSavings = +promoPrice;
+			}
+		}
 	}
 
-    cart.finalDis = 0;
-	updateSubtotal();
-	updateTotal();
+	// Hide cart and keep shopping
+	document.getElementById('keep-shopping').addEventListener('click', function(){
+		shopCart.classList.add('hide');
+		document.getElementById('display-cart').classList.remove('hide');
+	});
 
-}
+	// display cart, if items exist
+	document.getElementById('display-cart').addEventListener('click', function(){
+		shopCart.classList.remove('hide');
+		this.classList.add('hide');
+	});
 
+	// Promo Code application	
+	document.querySelector('.cart-item-promocode').addEventListener('blur', function(){
+		console.log('promo blue')
+		cartPricing.applyPromo(this.value);
+		if(cartPricing.promoSavings > 0){
+			this.parentNode.querySelector('.cart-item-price .price').innerHTML = cartPricing.promoSavings.toFixed(2);
+			displayPriceTotal(cartPricing.priceTotal);
+		}
+	})
 
-function updateQuantity(event) {
-    let parentRow = event.parentNode.parentNode;
-    let parentRowId = parentRow.id;
-    var inputQuantity,productPrice,updatedPrice,totalPrice;
-    
-    for (let node of parentRow.childNodes) {
-        switch (node.id) {
-            case 'quantity':
-                inputQuantity = node.firstChild;
-                break;
-                
-            case 'product-price':
-                productPrice = Number(node.innerHTML);
-                break;
-                
-            case 'updated-product-price':
-                updatedPrice = node;
-                totalPrice = Number(node.innerHTML);
-                console.log(totalPrice);
-        }
-    }
-    
-    let inputQuantityValue = (Number(inputQuantity.value)).toFixed(2);
-    
-    if (inputQuantityValue <= 0) removeFromCart(event);
-    
-    totalPrice = inputQuantity.value * productPrice;
-    updatedPrice.innerHTML = totalPrice;
-    
-    for (let item of cart.items) {
-        if (item.product === parentRowId) item.productUpdatedPrice = totalPrice;
-    }
-    
-    updateSubtotal();
-    updateTotal();
-    console.log(cart);
-}
+	// Add Item to Cart Functionality
+	for(var i = 0; i < addButtons.length; i++){
+		addButtons[i].addEventListener('click', function(){
+			//Grab item details
+			var itemName = this.parentNode.querySelector('.item-name').innerHTML;
+			var itemPrice = this.parentNode.querySelector('.item-price .price').innerHTML;
+			var itemImage = this.parentNode.querySelector('img').getAttribute('src');
+			
+			//build and append new item to cart
+			shopCart.querySelector('.shop-cart-list').appendChild(buildCartItem(itemName, itemPrice, itemImage));
+			//display cart if hidden
+			shopCart.classList.remove('hide');
+			// update pricing
+			displayPriceTotal(cartPricing.priceTotal);
+		})
+	}
 
-function applyPromo(event) {
-    let promoInputValue = document.getElementById('promo').value.toUpperCase();
-    let promoInput = document.getElementById('promo');
-    
-    switch (promoInputValue) {
-        case 'IPHONEX15':
-            let price = 0;
-            for (item of cart.items) {
-                if (item.product.includes('iphone-x')) {
-                    price += item.productPrice;
-                    let pro15Dis = (price * 15) / 100;
-                    
-                    cart.iphoneDis = pro15Dis;
-                    cart.finalDis = cart.iphoneDis.toFixed(2);
-                    addDiscountAmount();
-                }
-            }
-            
-            alert('15% Off the iPhone X!');
-            break;
-            
-        case 'ANDROID10':
-            for (item of cart.items) {
-                if (isIncluded('android', itemClass[item.product])) {
-                    let product = cartTableBody.querySelector('#' + item.product);
-                    var productUpdatedPrice = product.querySelector('#updated-product-price').innerHTML;
-                    
-                    productUpdatedPrice = (Number (productUpdatedPrice) * 10) / 100;
-                    
-                    product.querySelector('#updated-product-price').innerHTML = (Number (product.querySelector('#updated-product-price').innerHTML)) - productUpdatedPrice;
-                    
-                }
-            }
-            
-            cart.androidDis = productUpdatedPrice;
-            cart.finalDis = cart.androidDis.toFixed(2);
+	function buildCartItem(name, price, image){
+		// build element
+		var cartItem = document.createElement('li');
+		cartItem.className = 'cart-item';
 
-            addDiscountAmount();
-            
-            alert('10% Off all Android products!');
+		// add cart item to price list and set id
+		cartPricing.addItem(Object.keys(cartPricing.cartList).length + 1, price, 1);
+		cartItem.setAttribute('data-id', Object.keys(cartPricing.cartList).length);
 
-            break;
-            
-        case '5OFFEVERYTHING':
-            let subtotalDiscount = (cart.subtotal * 5) / 100;
-            
-            cart.Off5 = subtotalDiscount;
-            cart.finalDis = cart.Off5.toFixed(2);
-            addDiscountAmount();
-            
-            alert('5% Off the price of all products combined!');
-            break;
-            
-        case '':
-            alert('Please enter a discount code!');
-            
-            break;
-            
-        default:
-            alert('Invalid Code!');
-    }
-    
-    function addDiscountAmount() {
-        var orderTotalRow = document.getElementById("total-wrapper");
-        var orderTotalRowParent = orderTotalRow.parentNode; 
-        var newRow = document.createElement("tr");
-        orderTotalRowParent.insertBefore(newRow, orderTotalRow); 
-        newRow.setAttribute("id", "discountRow");
-        for(var j = 0; j < 2; j++){ 
-            var newTd = document.createElement("td"); 
-            newRow.appendChild(newTd);
-        }
-        newRow.childNodes[0].innerHTML = "Discount:";
-        newRow.childNodes[1].innerHTML = cart.finalDis;
-        newRow.childNodes[1].setAttribute("id", "discountAmt");
-    
-        updateSubtotal();
-        updateTotal();
-    }
-}
+		// Fill out cart item element
+		var cartImage = "<img src='"+image+"' alt=''>";
+		var cartName = "<a href='' class='cart-item-name'>" + name + "</a>";
+		var cartPrice = "<div class='cart-item-price'>$<span class='price'>"+ price +"</span></div>";
+		cartItem.innerHTML = cartImage + cartName +
+													"<input type='text' class='cart-item-quantity' placeholder='1' value='1'>"+
+													cartPrice+"<button class='cart-item-remove'>X</button>";
 
-function updateSubtotal() {
-    cart.subtotal = 0;
-    
-    for (let item of cart.items) {
-        cart.subtotal += item.productUpdatedPrice;
-    }
-    
-    subtotal.innerHTML = cart.subtotal.toFixed(2);
-}
+		//apply remove event
+		cartItem.querySelector('.cart-item-remove').addEventListener('click', function(){
+			removeCartItem(this.parentNode);
+		});
 
-function updateTotal() {
-    cart.total = 0;
-    cart.total += (cart.subtotal - cart.finalDis);
-    total.innerHTML = cart.total.toFixed(2);
-}
+		// Update cart on change of quantity;
+		cartItem.querySelector('.cart-item-quantity').addEventListener('blur', function(){
+			// remove item if quantity is 0
+			if(this.value == 0)
+				removeCartItem(this.parentNode);
+			else if(this.value){
+				// Update Pricing on quantity change
+				let itemPrice = cartPricing.cartList[this.parentNode.getAttribute('data-id')].price;
+				cartPricing.addItem(this.parentNode.getAttribute('data-id'), itemPrice, +this.value);
+				// Display new pricing
+				this.parentNode.querySelector('.cart-item-price .price').innerHTML = (itemPrice * +this.value).toFixed(2);
+				displayPriceTotal(cartPricing.priceTotal);
+			}
+		});
+		return cartItem;
+	}
 
-function isIncluded(input, words) {
-        try {
-            if(!input || words.length == 0) throw 'Invalid Input or words is empty';
-        }
+	function removeCartItem(node){
+		// update object
+		cartPricing.removeItem(node.getAttribute('data-id'));
+		// remove element
+		node.remove();
+		displayPriceTotal(cartPricing.priceTotal);
+		// if no items in cart, hide cart
+		if(Object.keys(cartPricing.cartList).length == 0)
+			shopCart.classList.add('hide');
+	}
 
-        catch(err) {
-            return `Please provide valid input or add a word to check for`;
-        }
-        
-        let allowed = [' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-        let count = 0;
-        
-        for (let index = 0; index < input.length; index++) {
-            if(!allowed.includes(input[index])) {
-                input = input.replace(input[index], ' ');
-            }
-        }
-        
-        input = input.split(' ');
-        
-        for (let i = 0; i < input.length; i++) {
-            for (let c = 0; c < 3; c++) {
-                if (input[i] == ' ' || input[i] == '') {
-                    input.splice(i, 1);
-                }
-            }
-        } 
-        
-        for (let word in input) {
-            for (let word2 in words) {
-                if (input[word] == words[word2]) return true;
-            }
-        }
-        return false;
-    }
+	function displayPriceTotal(price){
+		document.querySelector('.cart-total-num .price').innerHTML = price.toFixed(2);
+	}
+})(window);
